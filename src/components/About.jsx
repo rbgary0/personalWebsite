b33a7d77035bdc36444a4784827576bbe7e538f7
rbgary <rbgary@vt.edu>
2024-09-09 11:15:11 -0400

    small update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,8 +9,17 @@ function About() {
         <div className='about'>
             <div className='text'>
                 <img className='headshot' alt="headshot" src={headshot}></img>
-                <p>Hi! My name is Reagan Gary and I am currently a juior at Virginia Tech.
+                <p>Hi! My name is Reagan Gary and I am currently a junior at Virginia Tech.
                     I am majoring in computer science with an interest in software development.
+                    <br></br>
+                    <br></br>
+                    I built this website to share a little about myself and my computer science
+                    experience and skills, which you can find below.
+                    <br></br>
+                    <br></br>
+                    Linkedin: <a href="https://www.linkedin.com/in/reagangary">www.linkedin.com/in/reagangary</a>
+                    <br></br>
+                    Email: reagangary14@gmail.com
                 </p>
 
             </div>
