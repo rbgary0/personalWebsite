6aad8c70d1f657522ef2a0674953e858c0407c5b
rbgary <rbgary@vt.edu>
2024-09-09 22:56:55 -0400

    Updated Projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,7 @@
 import './projects.css'
-import logo from '/rg-high-resolution-logo.png'
+import logo from './rg-high-resolution-logo.png'
+import swipafy from './Spotify_logo_without_text.svg.png'
+import raspberry from './Raspberry.png'
 
 
 
@@ -10,8 +12,8 @@ function Projects() {
             <h1>Projects</h1>
                     <ul className='projectList'>
                         <li>
-                            <h3>This website</h3>
-                            <img alt="logo" src={logo}></img>
+                            <h3>Personal Website</h3>
+                            <img className='logo' alt="logo" src={logo}></img>
                             <p>I took a full stack development course last summer and used what I learned about
                             React to create this website. Showcases my skills and projects in Computer Science.</p>
                             <p>Languages/Tools Used: JavaScript, CSS, HTML, React</p>
@@ -19,8 +21,9 @@ function Projects() {
                         </li>
                         <li>
                             <h3>Swipafy</h3>
-                            <p>Programmed a website which utilizes Spotify’s API to collect data for the logged 
-                            in user to display their top artists and songs in a unique way. Allows users to discover 
+                            <img className='swipafy' alt="Swipafy" src={swipafy}></img>
+                            <p>I programmed a website which utilizes Spotify’s API to collect data for the logged 
+                            in user to display their top artists and songs in a unique way. It allows users to discover 
                             new songs using a “Tinder-like” format, which allows users to swipe left or right on 
                             new song recommendations.
                             </p>
@@ -28,6 +31,13 @@ function Projects() {
                         </li>
                         <li>
                             <h3>Wifi-Testing</h3>
+                            <img className='raspberry' alt="Raspberry Pi" src={raspberry}></img>
+                            <p>I worked with a small group of 4 students during senior year to develop a program in python to test the school network throughout the day to try to predict and detect outages.
+                            Placed Raspberry Pi’s around school to collect data.
+                            Collaborated with mentors, logged data, and analyzed results.
+                            Helped detect Wi-Fi dead spots around the school.
+                            </p>
+                            <p>Languages/Tools Used: Python, Raspberry Pi</p>
                         </li>
                     </ul>
         </div>
